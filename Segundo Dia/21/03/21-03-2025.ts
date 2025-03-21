3c334f87c541ddbe42f82dc710d6fc20812ebd21
Rubencalii <rubencorralromero2018@gmail.com>
2025-03-21 11:40:04 +0100

    mas ejercicios de practica

--- Segundo Dia/21/03/21-03-2025.ts
+++ Segundo Dia/21/03/21-03-2025.ts
@@ -75,3 +75,132 @@ class Coche{
 
 let numeros: number[] = [1, 2, 3, 4, 5];
 let texto: string[] = ['Hola', 'Mundo', 'este', 'es', 'mi', 'primer', 'Texto'];
+
+//Crea una función genérica que reciba dos parámetros de tipos diferentes y los combine en un solo objeto.
+
+function combinar<T, U>(param1: T, param2: U): { primera: T, segunda: U } {
+    return { primera: param1, segunda: param2 };
+}
+
+let resultado1 = combinar("Hola", 10);
+console.log(resultado); 
+
+//Crea una función que acepte un parámetro que pueda ser tanto un número como una cadena de texto. La función debe devolver un mensaje indicando el tipo del parámetro.
+
+function verificarTipo(valor: number| string):string{
+    if(typeof valor === 'number'){
+        return 'Es un numero';
+    } else{
+        return 'Es una cadena de texto.';
+    }
+}
+
+console.log(verificarTipo(123));
+console.log(verificarTipo('Hola'));
+
+//Crea un enum llamado Color con los valores Rojo, Verde y Azul. Luego, crea una variable que almacene un color y usa el enum para asignar el valor.
+
+enum Color{
+    Rojo = 'Rojo',
+    Verde = 'Verde',
+    Azul = 'Azul',
+}
+
+let colorFavorito: Color = Color.Azul;
+console.log(colorFavorito);
+
+// Crea una función que imprima un mensaje en la consola, pero que no devuelva ningún valor. Usa void como tipo de retorno.
+
+function saludar(nombre: string): void{
+    console.log(`Hola, ${nombre}!`)
+}
+
+saludar("Carlos");
+
+//Crea una función que acepte un parámetro de tipo unknown y maneje el valor de manera segura usando un tipo de comprobación.
+
+function manejarValor(valor: unknown): void{
+    if (typeof valor === "string"){
+        console.log(`El valor es una cadena: ${valor}`)
+    } else if (typeof valor === 'number'){
+        console.log('El valor es un numero: ' + valor );
+    } else {
+        console.log('El valor tiene un tipo desconocido.')
+    }
+}
+
+manejarValor('Texto');
+manejarValor(20);
+manejarValor(true);
+
+//Crea una función que calcule el factorial de un número de manera recursiva.
+
+function factorial(n: number): number{
+    if(n <= 1){
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
+
+//Crea una arrays que almacene un nombre (como string) y una edad (como number). Accede a sus valores e imprímelos.
+
+let persona1: [string, number] = ['Ruben', 20];
+
+console.log(persona1[0]);
+console.log(persona[1]);
+
+//Usa el operador as para hacer una aserción de tipo, y haz que TypeScript trate una variable como un tipo específico.
+
+let valor: any ='Hola Mundo';
+let longitud1: number = (valor as string).length;
+
+console.log(longitud1);
+
+//Crea una función que reciba dos parámetros, pero el segundo parámetro debe ser opcional. Si no se proporciona, debe usar un valor predeterminado.
+
+function saludar1(nombre: string, edad?: number): void {
+    if (edad) {
+        console.log(`Hola, ${nombre}, tienes ${edad} años.`);
+    } else {
+        console.log(`Hola, ${nombre}.`);
+    }
+}
+
+saludar("Carlos");
+saludar("Ana");        
+
+//Usa type para crear un alias de tipo que represente un objeto con las propiedades nombre y edad.
+
+type Persona2 = {
+    nombre: string;
+    edad: number;
+};
+
+let Persona3: Persona2 = {
+    nombre: 'Ruben',
+    edad: 20
+}
+
+console.log(persona.nombre);
+console.log(persona.edad);
+
+//Crea una clase llamada Rectángulo que tenga un método area que calcule y devuelva el área del rectángulo.
+
+class Rectangular{
+    ancho: number;
+    alto: number;
+
+    constructor(ancho: number, alto:number){
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+    
+    area(): number{
+        return this.ancho * this.alto;
+    }
+}
+
+let rect= new Rectangular(5, 10);
+console.log(rect.area());
