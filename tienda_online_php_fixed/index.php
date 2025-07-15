e52a799b15a9e1d1ff07fea361b06c75307c94a0
Rubencalii <rubencorralromero2018@gmail.com>
2025-07-15 11:42:51 +0200

    acabado

--- tienda_online_php_fixed/index.php
+++ tienda_online_php_fixed/index.php
@@ -31,13 +31,13 @@
                         <button id="todos" class="boton-menu boton-categoria active"><i class="bi bi-hand-index-thumb-fill"></i> Todos los productos</button>
                     </li>
                     <li>
-                        <button id="abrigos" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Viento</button>
+                        <button id="abrigos" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Cuerda</button>
                     </li>
                     <li>
-                        <button id="camisetas" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Percusion</button>
+                        <button id="camisetas" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Viento</button>
                     </li>
                     <li>
-                        <button id="pantalones" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Cuerda</button>
+                        <button id="pantalones" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Percusion</button>
                     </li>
                     <li>
                         <a class="boton-menu boton-carrito" href="./carrito.html">
